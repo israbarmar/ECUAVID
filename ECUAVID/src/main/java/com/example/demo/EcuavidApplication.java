5c00bef24ed28f2ff2d8f223554bf539934609cd
israbarmar <barbotoi@yahoo.com>
2020-07-02 11:16:17 -0500

    "Commit message"

--- ECUAVID/src/main/java/com/example/demo/EcuavidApplication.java
+++ ECUAVID/src/main/java/com/example/demo/EcuavidApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+
+@EntityScan( basePackages = {"security.entity"} )
 public class EcuavidApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcuavidApplication.class, args);
+		
 	}
 
 }
